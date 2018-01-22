d9f4ae14869e21fa02971df11c0b4d766e488065
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2018-01-22 23:12:55 +0200

    Refactored layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, button, div, text, label, input, h3)
+import Html exposing (Html, button, div, text, label, input, h1, h3)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (placeholder, value, class, type_)
 import WordGenerator exposing (lettersToWords)
@@ -49,21 +49,39 @@ view: Model -> Html Msg
 
 view model =
   div [ class "content" ]
-    [ div [class "inputs"]
+    [ h1 [] [ text "Letters to words" ]
+    , div [class "inputs"]
       [ input [ type_ "text", value model.input, onInput InputChanged ] []
         , button [ class "button", onClick Letter_Added ] [ text "Add" ]
       ]
-      , h3 [] [ text "You've added these letters:" ]
-      , div [class "letters"] (List.map renderLetter model.letters)
-      , h3 [] [ text "The generated words are: "]
-      , div [class "words"] (List.map renderWord (List.sort model.words))
+      , renderLetterArea model.letters
+      , renderWords model.words
     ]
+
+renderLetterArea: List String -> Html Msg
+renderLetterArea letters =
+  case letters of
+    [] -> div [] []
+    _ -> div [] [
+      h3 [] [ text "You've added these letters:" ]
+      , div [class "letters"] (List.map renderLetter letters)
+    ]
+
 renderLetter: String -> Html Msg
 renderLetter letter =
   div [ class "letter" ]
   [
     text letter
   ]
+
+renderWords: List String -> Html Msg
+renderWords words =
+  case words of
+    [] -> div [] []
+    _ -> div [] [
+      h3 [] [ text "The generated words are: "]
+      , div [class "words"] (List.map renderWord (List.sort words))
+    ]
 
 renderWord: String -> Html Msg
 renderWord word =
