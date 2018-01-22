0ec37678f04f1784691e045238ef42338ed7a791
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2018-01-22 22:11:34 +0200

    Modified update to calculate available words when letter is added

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, button, div, text, label, input)
+import Html exposing (Html, button, div, text, label, input, h3)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (placeholder, value, class, type_)
 import WordGenerator exposing (lettersToWords)
@@ -31,11 +31,14 @@ update: Msg -> Model -> Model
 update msg model =
     case msg of
       Letter_Added ->
-        { model |
-          letters = List.append model.letters [model.input]
-          , words = lettersToWords model.letters
-          , input = ""
-        }
+        let
+          withInput = List.append model.letters [model.input]
+        in
+          { model |
+            letters = withInput
+            , words = lettersToWords withInput
+            , input = ""
+          }
 
       InputChanged value ->
         { model | input = value }
@@ -50,7 +53,10 @@ view model =
       [ input [ type_ "text", value model.input, onInput InputChanged ] []
         , button [ class "button", onClick Letter_Added ] [ text "Add" ]
       ]
+      , h3 [] [ text "You've added these letters:" ]
       , div [class "letters"] (List.map renderLetter model.letters)
+      , h3 [] [ text "The generated words are: "]
+      , div [class "words"] (List.map renderWord (List.sort model.words))
     ]
 renderLetter: String -> Html Msg
 renderLetter letter =
@@ -58,3 +64,7 @@ renderLetter letter =
   [
     text letter
   ]
+
+renderWord: String -> Html Msg
+renderWord word =
+  div [class "word"] [ text word ]
